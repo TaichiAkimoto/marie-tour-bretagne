d722138ae34254b4a8111648b2773bb557f99eac
穐元太一 <taichi@taichis-MacBook-puro.local>
2019-03-30 00:20:13 +0900

    add first section

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as style from "./HeroSection.module.scss";
+import * as classNames from "classnames";
+
+interface Props {}
+interface State {}
+
+export class HeroSection extends React.Component<Props, State> {
+  render() {
+    return (
+      <section className={style.HeroSection}>
+        <div className={style.backgroundLayer}>
+          <img src={require("../../assets/signboard.jpg")} />
+        </div>
+      </section>
+    );
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { graphql } from "gatsby";
 import * as React from "react";
 import * as styles from "./Index.module.scss";
+import { HeroSection } from "../components/HeroSection/HeroSection";
 
 interface IndexPageProps {
   data: {
@@ -26,13 +27,10 @@ export const indexPageQuery = graphql`
 
 export default class IndexPage extends React.Component<IndexPageProps, {}> {
   public render() {
-    const { name, tagline } = this.props.data.site.siteMetadata;
-
     return (
-      <div className={styles.Container}>
-        <h1>{name}</h1>
-        <p>{tagline}</p>
-      </div>
+      <>
+        <HeroSection />
+      </>
     );
   }
 }
