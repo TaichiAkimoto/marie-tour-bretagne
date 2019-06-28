8cf60bffa5e8447689e8c348b86e0650a43c2d06
穐元太一 <taichi@taichis-MacBook-puro.local>
2019-06-28 18:48:56 +0900

    implement: herosection

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -9,11 +9,25 @@ export class HeroSection extends React.Component<Props, State> {
   render() {
     return (
       <section className={style.HeroSection}>
-        <div className={style.backgroundLayer}>
-          <img src={require("../../assets/signboard.jpg")} />
+        <div className={style.container}>
+          <img
+            src={require("../../assets/title.jpg")}
+            className={style.title}
+          />
         </div>
-        <div>
-          let's begin to develop this beautiful website, shall we?
+        <div className={style.buttons}>
+          <img
+            src={require("../../assets/instagram.jpg")}
+            className={style.instagram}
+          />
+          <img
+            src={require("../../assets/facebook.jpg")}
+            className={style.facebook}
+          />
+          <img
+            src={require("../../assets/email.jpg")}
+            className={style.email}
+          />
         </div>
       </section>
     );
